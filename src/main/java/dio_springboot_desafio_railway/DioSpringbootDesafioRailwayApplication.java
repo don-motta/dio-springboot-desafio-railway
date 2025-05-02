b42666893545d950622126e88124b1f74bc89684
donx14 <alx_m.almeida@hotmail.com>
2025-05-02 10:32:22 -0300

    Initial commit

---
+++ src/main/java/dio_springboot_desafio_railway/DioSpringbootDesafioRailwayApplication.java
@@ -0,0 +1,13 @@
+package dio_springboot_desafio_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootDesafioRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootDesafioRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio_springboot_desafio_railway/DioSpringbootDesafioRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_springboot_desafio_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootDesafioRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
